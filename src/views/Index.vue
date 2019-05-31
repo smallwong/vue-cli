1d9bef3e9f7c49890e773d3606cc02d7dec8e698
wong <wong@jfetek.com>
2019-05-31 16:28:17 +0800

    1.add Index.vue 2.new console

---
+++ src/views/Index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="home">
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+// eslint-disable-next-line semi
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+    name: 'index',
+    components: {
+        HelloWorld
+    },
+    created () {
+    /* eslint-disable */
+    console.log(`          ,@@@@#@@.;,`);
+    console.log(`        .@@@@@@@@@@@@@@+ `);
+    console.log(`       @@@@@@@@@@@@@@@@@@#`);
+    console.log(`     '@@@@@@@@@@@@@@@@@@@@@/`);
+    console.log(`    .@@@@@@@@@@@@@@@@@@@@@@@#`);
+    console.log(`   @@@@@@@@@@. @@@@@@@@@@@@@@@`);
+    console.log(`  .@@@@@@@@:    @@   ;#+@@@@@@`);
+    console.log(`  @@@@@@+                @@@@@@`);
+    console.log(` '@@@@                     @@@@`);
+    console.log(` ,@@@,                     @@@@@`);
+    console.log(` ;@@@                       @@@@`);
+    console.log(` +@@@                       @@@@`);
+    console.log(` +@@@                       @@@@`);
+    console.log(` ;@@@'                       @@;`);
+    console.log(`  @@'                        +@`);
+    console.log(`  @                          @ `);
+    console.log(`  @,                         @`);
+    console.log(`  @,                         @`);
+    console.log(`  #@                        ;#`);
+    console.log(`   @                        @ `);
+    console.log(`   ';                       @`);
+    console.log(`   .@@@@      :@;#      @@@@@`);
+    console.log(`   .@@@@      :@;#      @@@@@`);
+    console.log(`    @@@@   @@@@@@@@@@:  ,@@@@`);
+    console.log(`    @@@@   @@@@@@@@@@:  ,@@@@`);
+    console.log(`    #@@@ '@;         @@ .@@@ `);
+    console.log(`    #@@@ '@;         @@ .@@@ `);
+    console.log(`    @@@ @@          .@'+@@,`);
+    console.log(`      ,@@@@+ @@@@@@  @@@@@`);
+    console.log(`      @@@@@ @@@@@@  @@@@ `);
+    console.log(`        @@@@@@@@@@@@@@' `);
+    console.log(`          ;@@@@@@@@@ `);
+    console.log(`             ,,:,`);
+    console.log(``);
+    console.log(`       John Wick在此，Bug不得撒野`);
+    /* eslint-enable */
+    }
+}
+</script>
+<style>
+</style>
